dc9302250413be8f9119c1a74dced07843a77691
Michael Barillas <michaelbarillas91@gmail.com>
2015-08-01 11:55:07 -0700

    Forgot to make a repo before starting
    Adding completed solution.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Main main = new Main();
+		System.out.println("Starting Even Fibonacci Numbers");
+		
+		System.out.println("End of Even Fibonacci Numbers");
+	}
+
+}
